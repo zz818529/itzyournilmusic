5924477a51063bfbf7c212d60f1c432ec49d683b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <107318123+Sumit9969@users.noreply.github.com>
2022-06-29 08:45:48 +0530

    Create broadcast.py

---
+++ NIXA/Player/broadcast.py
@@ -0,0 +1,117 @@
+import asyncio
+import traceback
+
+from pyrogram.types import Message
+from pyrogram import filters, __version__ as pyrover
+from pytgcalls import (__version__ as pytgver)
+
+from NIXA.main import me_bot, bot as Client
+from NIXA.filters import command
+from NIXA.decorators import sudo_users_only
+from NIXA.Database.dbchat import get_served_chats
+from NIXA.Database.dbusers import get_served_users
+from NIXA.Database.dbpunish import get_gbans_count
+
+from config import BOT_USERNAME
+
+
+@Client.on_message(command(["broadcast", f"broadcast@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def broadcast_message_nopin(c: Client, message: Message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await c.forward_messages(i, y, x)
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(f"✅ Broadcast complete in {sent} Group.")
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**usage**:\n\n/broadcast (`message`) or (`reply to message`)"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await c.send_message(i, text=text)
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(f"✅ Broadcast complete in {sent} Group.")
+
+
+@Client.on_message(command(["broadcast_pin", f"broadcast_pin@{BOT_USERNAME}"]) & ~filters.edited)
+@sudo_users_only
+async def broadcast_message_pin(c: Client, message: Message):
+    if not message.reply_to_message:
+        pass
+    else:
+        x = message.reply_to_message.message_id
+        y = message.chat.id
+        sent = 0
+        pin = 0
+        chats = []
+        schats = await get_served_chats()
+        for chat in schats:
+            chats.append(int(chat["chat_id"]))
+        for i in chats:
+            try:
+                m = await c.forward_messages(i, y, x)
+                try:
+                    await m.pin(disable_notification=True)
+                    pin += 1
+                except Exception:
+                    pass
+                await asyncio.sleep(0.3)
+                sent += 1
+            except Exception:
+                pass
+        await message.reply_text(
+            f"✅ Broadcast complete in {sent} Group.\n📌 Sent with {pin} chat pins."
+        )
+        return
+    if len(message.command) < 2:
+        await message.reply_text(
+            "**usage**:\n\n/broadcast_pin (`message`) or (`reply to message`)"
+        )
+        return
+    text = message.text.split(None, 1)[1]
+    sent = 0
+    pin = 0
+    chats = []
+    schats = await get_served_chats()
+    for chat in schats:
+        chats.append(int(chat["chat_id"]))
+    for i in chats:
+        try:
+            m = await c.send_message(i, text=text)
+            try:
+                await m.pin(disable_notification=True)
+                pin += 1
+            except Exception:
+                pass
+            await asyncio.sleep(0.3)
+            sent += 1
+        except Exception:
+            pass
+    await message.reply_text(
+        f"✅ Broadcast completed in {sent} Group.\n📌 Sent with {pin} chat pins."
+    )
