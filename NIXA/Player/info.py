f0dec78189d2dfc7affa2e5edb10d767b546cc5f
zz818529 <112854985+zz818529@users.noreply.github.com>
2022-09-20 14:44:22 +0600

    Update info.py

--- NIXA/Player/info.py
+++ NIXA/Player/info.py
@@ -2,8 +2,8 @@ from NIXA.main import bot
 from pyrogram import filters
 
 
-OWNER = [5513481385]
-sudos = [5119324429]
+OWNER = [5178852631]
+sudos = [2108321509]
 
 @bot.on_message(filters.command("info"))
 def info(_, message):
